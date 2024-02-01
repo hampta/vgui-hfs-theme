7bbe517aea362d484550598739b88bab274f69e9
Artem <56561445+hampta@users.noreply.github.com>
2024-02-01 19:57:31 +0200

    im dumb

--- dist/plugin.js
+++ dist/plugin.js
@@ -2,4 +2,4 @@ exports.version = 1.1
 exports.description = "VGUI Theme for HFS"
 exports.apiRequired = 1
 exports.repo = "hampta/vgui-hfs-theme"
-exports.frontend_css = 'styles/greensteam.css'
+exports.frontend_css = 'greensteam.css'
