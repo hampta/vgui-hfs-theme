409585755df567acb41fa688232ebfca2109e052
Artem <56561445+hampta@users.noreply.github.com>
2024-02-01 19:56:30 +0200

    Remove unused images and styles

--- dist/plugin.js
+++ dist/plugin.js
@@ -1,5 +1,5 @@
-exports.version = 1.0
+exports.version = 1.1
 exports.description = "VGUI Theme for HFS"
 exports.apiRequired = 1
 exports.repo = "hampta/vgui-hfs-theme"
-exports.frontend_css = 'styles/greensteam/greensteam.css'
+exports.frontend_css = 'styles/greensteam.css'
