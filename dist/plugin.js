2486c7fab6aa1b257d665ddc03a617db602ad1f1
Artem <56561445+hampta@users.noreply.github.com>
2024-02-01 17:02:49 +0200

    Update repository and CSS styles

--- dist/plugin.js
+++ dist/plugin.js
@@ -1,5 +1,5 @@
 exports.version = 1.0
 exports.description = "VGUI Theme for HFS"
 exports.apiRequired = 1
-exports.repo = "hampta/VGUI-Theme"
-exports.frontend_css = '/styles/greensteam/greensteam.css'
+exports.repo = "hampta/vgui-hfs-theme"
+exports.frontend_css = 'styles/greensteam/greensteam.css'
