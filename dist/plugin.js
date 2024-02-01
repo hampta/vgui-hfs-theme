fb32ba05088be0994768b1e021273738f83bc460
Artem <56561445+hampta@users.noreply.github.com>
2024-02-01 16:32:03 +0200

    first commit

---
+++ dist/plugin.js
@@ -0,0 +1,5 @@
+exports.version = 1.0
+exports.description = "VGUI Theme for HFS"
+exports.apiRequired = 1
+exports.repo = "hampta/VGUI-Theme"
+exports.frontend_css = '/styles/greensteam/greensteam.css'
